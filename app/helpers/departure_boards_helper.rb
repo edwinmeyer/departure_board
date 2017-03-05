ba9cd57198c145db5adb9570b70acb819fcdfa38
Edwin Meyer <edwin@edwinmeyer.com>
2017-03-04 19:43:24 -0700

    improve page rendering; add departure sorting & limiting

--- app/helpers/departure_boards_helper.rb
+++ app/helpers/departure_boards_helper.rb
@@ -1,5 +1,7 @@
 module DepartureBoardsHelper
 
+  MAX_DISPLAYED_DEPARTURES = 12
+
   def epoch_to_eastern_time(epoch_time)
     # config.time_zone = "Eastern Time (US & Canada)"
     Time.at(epoch_time.to_i).in_time_zone("Eastern Time (US & Canada)").strftime( "%l:%M %p" )
@@ -7,5 +9,9 @@ module DepartureBoardsHelper
 
   def track_num(track_num)
     track_num ? track_num : "TBD"
+  end
+
+  def sort_limit(departures)
+    departures.sort {|d1, d2| d1.scheduledtime <=> d2.scheduledtime}[0...MAX_DISPLAYED_DEPARTURES]
   end
 end
